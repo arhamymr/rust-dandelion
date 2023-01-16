6353df2f673bd4baec7d253f3b0c04ff5d1daf88
muhammad arham <arhamymr@gmail.com>
2023-01-16 21:56:28 +0800

    feat: CLI and simple loop to files

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,25 @@
+#![allow(unused)]
+
+use clap::Parser;
+use std::fs;
+use std::io;
+
+#[derive(Parser)]
+struct Cli {
+    pattern: String,
+    path: std::path::PathBuf,
+}
+
 fn main() {
-    println!("Hello, world! tes ts ss fsf est ahaaha");
+    let args = Cli::parse();
+    let mut content = String::new();
+
+    if &args.pattern == "parse" {
+        let content = fs::read_to_string(&args.path).expect("could not read file");
+        let content_chars = content.chars();
+
+        for c in content_chars {
+            println!("content {c}")
+        }
+    }
 }
